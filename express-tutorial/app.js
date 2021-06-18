fb6c78ff0e4fdaa57878e1be98487ef35534dc67
Noel Bray <nwbray-code@yahoo.com>
2021-06-18 14:01:12 -0400

    Modify App.js In Express-Tutorial Folder
    I added a comment to remind myself to make sure I'm in the express-tutorial folder before running the app with npm start (script: 'nodemon app.js') or node app.js or any other way.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,1 +1,3 @@
+// Don't forget to make sure you are in the express-tutorial folder before launching the app through node app.js or npm start (nodemon script: nodemon app.js)
+
 console.log("\nExpress Tutorial -- Express App Is Working --\n\n");
