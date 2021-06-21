955dbc74c94087bd4883605eca43568280252f8c
Noel Bray <nwbray-code@yahoo.com>
2021-06-21 14:48:31 -0400

    Add 1 File And Modify 2 Files
        - 10-middleware-options.js
        - package-lock.json
        - package.json
    
    10-middleware-options.js briefly demonstrates the other two middleware options:
    
    1.  Create Your Own Middleware Function / Module
      -- demonstrated in 8-middleware-basics.js and 9-middleware-use.js
    2. Express Built-in Middleware
    3. Third Party Middleware From NPM
    
    The morgan middleware on NPM was installed and package-lock.json and package.json were automatically updated as a result.

---
+++ express-tutorial/10-middleware-options.js
@@ -0,0 +1,103 @@
+// Notes: 
+// Middleware Function Options - create our own / express built-in / third party
+
+// Middleware is all throughout Express JS
+// Middle are functions that execute during the request to the server. Each middleware has access to the req and res object.
+
+// When you work with middleware, you Must pass it on to the next middleware unless you are terminating the whole cycle by sending back a response, res.
+
+const express = require('express');
+
+const app = express();
+const morgan = require('morgan');
+
+const logger = require('./logger-middleware.js');
+
+const authorize = require('./authorize-middleware');
+
+// 1. Middleware use method vs route 
+
+// req => middleware => res
+// Middleware sits in between.
+// Request comes in, do something, then send response.
+
+// Apply Middleware To Multiple Routes:
+// use the Express method .use()
+// Order Matters Here!!!!
+// the .use() method has to be placed before all routes that you want it to apply to.
+// All app.use()s and middleware functions must be at above the browser methods such as get and post that will use them
+// .use() methods are usually placed at the top of the document.
+
+// app.use(logger);
+
+//Execute Multiple Middleware, Place them in an array in the use method, and the Middleware Functions will be executed in the order they are in the array:
+
+// app.use([authorize, logger]); // 
+
+// You can type path into the use() method and it will be applied to anything, route, path after that:
+
+// app.use('/api', logger); 
+
+// The above use method will be applied only to all routes that have /api/and-all-paths-after-it
+
+// Learn More About Express's Use() Method:
+// https://expressjs.com/en/4x/api.html#app.use
+
+// Express Built-in Middleware Function, static() method:
+// app.use(express.static('./public'));
+
+// Third Party Middleware Function:
+// Read How To Use Morgan Middleware: https://www.npmjs.com/package/morgan
+app.use(morgan('tiny'));
+
+app.get(
+    '/',
+    // logger,// Middleware function
+    (req, res) => {
+        // const method = req.method;
+        // const url = req.url;
+        // const time = new Date().getFullYear();
+
+        // console.log("method: ", method, "url: ", url, "time: ", time);
+
+        res.send('Home Page');
+    }
+);
+
+app.get(
+    '/about',
+    // logger,// Middlware function
+    (req, res) => {
+        res.send('About Page');
+    }
+);
+
+app.get(
+    '/api/products',
+    // logger,
+    (req, res) => {
+        console.log(req.user);
+        
+        res.send('Products');
+    }
+);
+
+app.get(
+    '/api/items',
+    // logger,
+    // [logger, authorize], // Add Multiple Middleware Functions
+    (req, res) => {
+        console.log(req.user);
+
+        res.send('Items')
+    }
+);
+
+const port = 5000;
+
+app.listen(
+    port,
+    () => {
+        console.log('\nThis Express App Concerning Middleware "Use" Method Is Working. TYGG.\n')
+    }
+);
